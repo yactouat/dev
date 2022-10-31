db263c8b6c56f2d305568901c31a77174f421429
yactouat <yactouat@hotmail.com>
2022-10-31 19:54:10 +0100

    files clearing is more generic now

---
+++ src/DevFilesManager.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Yactouat\Dev;
+
+/**
+ * this class is responsible for handling files during the development phase
+ */
+final class DevFilesManager
+{
+
+    /**
+     * clears all files with specified folders except .gitkeep and .gitignore
+     *
+     * @param array $folders
+     * @return void
+     */
+    public static function clearFolders(array $folders): void {
+        foreach ($folders as $folder) {
+            if (is_dir($folder)) {
+                $files = glob($folder.'/*');
+                foreach ($files as $file) {
+                    if (is_file($file) && !str_ends_with($file, "gitkeep") && !str_ends_with($file, "gitignore")) {
+                        unlink($file);
+                    }
+                }
+            }
+        }
+    }
+}
